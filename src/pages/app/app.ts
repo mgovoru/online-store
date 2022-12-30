bec335ae9b99ad2777b62c6ce7b4163883639150
Mark Kulishov <>
2022-12-30 23:01:28 +0200

    feat: refactor folders. Add error page

---
+++ src/pages/app/app.ts
@@ -0,0 +1,65 @@
+import Header from '../../core/components/header/index';
+import Footer from '../../core/components/footer/index';
+import Page from '../../core/templates/pages';
+import MainPage from '../main/index';
+import ErrorPage, { ErrorTypes } from '../error/index';
+
+
+ export const enum PageIds {
+     MainPage = 'main-page',
+     CatalogPage = 'catalog-page',
+     CartPage = 'cart-page',
+ }
+
+class App {
+    private static container: HTMLElement = document.body;
+    private static defaultPageId: string = 'current-page';
+    private header: Header;
+    private footer: Footer;
+     static renderNewPage(idPage: string) {
+         const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
+         if (currentPageHTML) {
+             currentPageHTML.remove();
+         }
+         let page: Page | null = null;
+
+         if (idPage === PageIds.MainPage) {
+             page = new MainPage(idPage);
+         }
+        //  } else if (idPage === PageIds.SettingsPage) {
+        //      page = new CatalogPagePage(idPage);
+        //  } else if (idPage === PageIds.StatisticsPage) {
+        //      page = new CartPage(idPage);
+        //  }
+      //   else {
+             page = new ErrorPage(idPage, ErrorTypes.Error_404);
+        // }
+
+         const pageHTML = page.render();
+         pageHTML.id = App.defaultPageId;
+         App.container.append(pageHTML);
+     }
+
+    private enableRouteChange() {
+        window.addEventListener('hashchange', () => {
+            const hash = window.location.hash.slice(1);
+            App.renderNewPage(hash);
+        });
+    }
+
+    constructor() {
+        this.header = new Header('header', 'header');
+        this.footer = new Footer('footer', 'footer');
+    }
+
+    run() {
+        App.container.append(this.header.render());
+         App.renderNewPage('main-page');
+        // this.enableRouteChange();
+        App.container.append(this.footer.render());
+    }
+}
+
+// Main, Settings, Statistics
+
+export default App;
