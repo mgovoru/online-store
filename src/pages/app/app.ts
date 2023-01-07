400e87b6a7297516d1327b88c192da502e51ab07
mgovoru <m.govoru@yandex.ru>
2023-01-07 19:14:38 +0500

    add: modules and function

--- src/pages/app/app.ts
+++ src/pages/app/app.ts
@@ -1,44 +1,51 @@
 import Header from '../../core/components/header/index';
 import Footer from '../../core/components/footer/index';
+import Main from '../../core/components/maindiv/index';
 import Page from '../../core/templates/pages';
 import MainPage from '../main/index';
+import CatalogPage from '../catalog/index';
 import ErrorPage, { ErrorTypes } from '../error/index';
 
-
- export const enum PageIds {
-     MainPage = 'main-page',
-     CatalogPage = 'catalog-page',
-     CartPage = 'cart-page',
- }
+export const enum PageIds {
+    MainPage = 'main-page',
+    CatalogPage = 'catalog-page',
+    CartPage = 'cart-page',
+}
 
 class App {
     private static container: HTMLElement = document.body;
     private static defaultPageId: string = 'current-page';
     private header: Header;
     private footer: Footer;
-     static renderNewPage(idPage: string) {
-         const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
-         if (currentPageHTML) {
-             currentPageHTML.remove();
-         }
-         let page: Page | null = null;
+    private main: Main;
+    static renderNewPage(idPage: string) {
+        const currentPageHTML = document.querySelector(`#${App.defaultPageId}`);
+        console.log(currentPageHTML);
+        if (currentPageHTML) {
+            console.log('yes');
+            currentPageHTML.remove();
+            console.log(document.querySelector(`#${App.defaultPageId}`));
+        }
+        let page: Page | null = null;
 
-         if (idPage === PageIds.MainPage) {
-             page = new MainPage(idPage);
-         }
-        //  } else if (idPage === PageIds.SettingsPage) {
-        //      page = new CatalogPagePage(idPage);
-        //  } else if (idPage === PageIds.StatisticsPage) {
-        //      page = new CartPage(idPage);
-        //  }
-      //   else {
-             page = new ErrorPage(idPage, ErrorTypes.Error_404);
-        // }
+        if (idPage === PageIds.MainPage) {
+            page = new MainPage(idPage);
+        }
+        else if (idPage === PageIds.CatalogPage) {
+            page = new CatalogPage(idPage);
+            console.log(document.querySelector(`#${App.defaultPageId}`));
+            console.log(page);
+            //  } else if (idPage === PageIds.StatisticsPage) {
+            //      page = new CartPage(idPage);
+        }
+        else {
+            page = new ErrorPage(idPage, ErrorTypes.Error_404);
+        }
 
-         const pageHTML = page.render();
-         pageHTML.id = App.defaultPageId;
-         App.container.append(pageHTML);
-     }
+        const pageHTML = page.render();
+        pageHTML.id = App.defaultPageId;
+        App.container.append(pageHTML);
+    }
 
     private enableRouteChange() {
         window.addEventListener('hashchange', () => {
@@ -49,13 +56,14 @@ class App {
 
     constructor() {
         this.header = new Header('header', 'header');
+        this.main = new Main('main', 'main');
         this.footer = new Footer('footer', 'footer');
     }
 
     run() {
         App.container.append(this.header.render());
-         App.renderNewPage('main-page');
-        // this.enableRouteChange();
+        App.renderNewPage('main-page');
+        this.enableRouteChange();
         App.container.append(this.footer.render());
     }
 }
---
+++ src/pages/catalog/index.ts
@@ -0,0 +1,30 @@
+import Page from '../../core/templates/pages';
+class CatalogPage extends Page {
+
+	constructor(id: string) {
+		super(id);
+	}
+	createElementMain(tagName: string, className: string, elementMain: HTMLElement, classNameTwo?: string) {
+		let element = document.createElement(tagName);
+		element.classList.add(className);
+		if (classNameTwo != null) { element.classList.add(classNameTwo) };
+		elementMain.append(element);
+		return element;
+	}
+
+
+	renderMain() {
+		this.container.classList.add('main');
+		const mainContainer = this.createElementMain('div', 'main__container', this.container);
+		const mainMenu = this.createElementMain('div', 'main__menu', mainContainer, 'menu');
+		const menuItems = this.createElementMain('ul', 'menu__items', mainMenu);
+		const menuItem = this.createElementMain('li', 'menu__item', menuItems);
+		menuItem.innerHTML = 'STORE';
+	}
+	render() {
+		this.renderMain();
+		return this.container;
+	}
+}
+
+export default CatalogPage;
